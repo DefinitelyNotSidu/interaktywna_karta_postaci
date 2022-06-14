9919558270af73a11afcc0632eba09bb62812405
DefinitelyNotSidu <b.magdz@gmail.com>
2022-06-14 18:58:31 +0200

    creating modifier handler

--- lib/models/stat.dart
+++ lib/models/stat.dart
@@ -3,6 +3,27 @@ import 'package:flutter/services.dart';
 
 double size = 30.0;
 
+//
+class StatValues {
+  String statType = "Siła";
+  int value = 0;
+  int _modifier = 0;
+
+  void setModifier() {
+    if (value.isEven) {
+      _modifier = (value - 10) ~/ 2;
+    } else {
+      _modifier = (value - 11) ~/ 2;
+    }
+  }
+
+  int getModifier() {
+    return _modifier;
+  }
+}
+
+StatValues str = StatValues();
+
 class Strength extends StatefulWidget {
   const Strength({Key? key}) : super(key: key);
 
@@ -11,7 +32,7 @@ class Strength extends StatefulWidget {
 }
 
 class _StrengthState extends State<Strength> {
-  String statType = 'Siła';
+  /*String statType = 'Siła';
   int value = 0;
   int _modifier = 0;
   void setModifier() {
@@ -24,16 +45,7 @@ class _StrengthState extends State<Strength> {
 
   int getModifier() {
     return _modifier;
-  }
-
-  //text controller
-  final myController = TextEditingController();
-
-  @override
-  void dispose() {
-    myController.dispose();
-    super.dispose();
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -44,25 +56,36 @@ class _StrengthState extends State<Strength> {
         children: [
           //value
           TextField(
-            controller: myController,
-            decoration: InputDecoration(labelText: statType),
+            decoration: InputDecoration(labelText: str.statType),
             keyboardType: TextInputType.number,
             inputFormatters: <TextInputFormatter>[
               FilteringTextInputFormatter.digitsOnly
             ],
             onSubmitted: (text) {
-              value = int.parse(text);
-              setModifier();
+              str.value = int.parse(text);
+              str.setModifier();
             },
           ),
           //TODO: update modifier on value change
-          //modifier
-          Text(getModifier().toString())
+          Modifier()
         ],
       ),
     );
   }
 }
 
+//modifier handler
 
-//
+class Modifier extends StatefulWidget {
+  const Modifier({Key? key}) : super(key: key);
+
+  @override
+  State<Modifier> createState() => _ModifierState();
+}
+
+class _ModifierState extends State<Modifier> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(str.getModifier().toString());
+  }
+}
